eb6cc0125ff13265e5d35cc338f353f0523fff3d
Lucas Eklund <luckeeklund@gmail.com>
2023-04-20 06:54:47 +0300

    cool ex02 done with operator modifcations :O

---
+++ mod-02/ex02/src/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: leklund <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/18 10:18:18 by leklund           #+#    #+#             */
+/*   Updated: 2023/04/18 10:18:19 by leklund          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "Fixed.hpp"
+
+int main( void ) {
+Fixed a;
+Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+std::cout << a << std::endl;
+std::cout << ++a << std::endl;
+std::cout << a << std::endl;
+std::cout << a++ << std::endl;
+std::cout << a << std::endl;
+std::cout << b << std::endl;
+std::cout << Fixed::max( a, b ) << std::endl;
+	return (0);
+}
